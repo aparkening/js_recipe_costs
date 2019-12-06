ec75a2424bcf2656c34ada9011b0bb2d0105fe32
aparkening <aaron.parkening@gmail.com>
2019-12-06 14:16:18 -0500

    Replaced postgres with sqlite. Migration success.

---
+++ backend/db/schema.rb
@@ -0,0 +1,85 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2019_10_18_015824) do
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.float "cost"
+    t.float "cost_size"
+    t.string "cost_unit"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "recipe_ingredients", force: :cascade do |t|
+    t.integer "recipe_id"
+    t.integer "ingredient_id"
+    t.float "ingredient_amount"
+    t.string "ingredient_unit"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["ingredient_id"], name: "index_recipe_ingredients_on_ingredient_id"
+    t.index ["recipe_id"], name: "index_recipe_ingredients_on_recipe_id"
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "name"
+    t.integer "servings"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_recipes_on_user_id"
+  end
+
+  create_table "user_ingredient_costs", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "ingredient_id"
+    t.float "cost"
+    t.float "cost_size"
+    t.string "cost_unit"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["ingredient_id"], name: "index_user_ingredient_costs_on_ingredient_id"
+    t.index ["user_id"], name: "index_user_ingredient_costs_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
+    t.boolean "admin", default: false
+    t.string "organization"
+    t.string "uid"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "google_token"
+    t.string "google_refresh_token"
+  end
+
+  create_table "weight_volume_conversions", force: :cascade do |t|
+    t.integer "ingredient_id"
+    t.float "weight_size"
+    t.string "weight_unit"
+    t.float "vol_size"
+    t.string "vol_unit"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["ingredient_id"], name: "index_weight_volume_conversions_on_ingredient_id"
+  end
+
+  add_foreign_key "recipe_ingredients", "ingredients"
+  add_foreign_key "recipe_ingredients", "recipes"
+  add_foreign_key "user_ingredient_costs", "ingredients"
+  add_foreign_key "user_ingredient_costs", "users"
+  add_foreign_key "weight_volume_conversions", "ingredients"
+end
