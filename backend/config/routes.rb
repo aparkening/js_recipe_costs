64284faa5526c58b8c747c8782992fb789f9d709
aparkening <aaron.parkening@gmail.com>
2019-12-06 16:59:35 -0500

    Add initial route for recipes index.

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,3 +1,29 @@
 Rails.application.routes.draw do
+  # Root
+  # root 'application#index'
+
+  # Ingredients
+  # resources :ingredients
+  # post 'ingredients/import'
+
+  # Users, with recipes and user_ingredients
+  # resources :users do
+  #   resources :recipes
+  #   get 'recipes/ingredients/:id' => 'recipes#by_ingredient', as: "recipes_by_ingredient"
+  #   post 'recipes/import'
+  #   resources :ingredients, controller: 'user_ingredient_costs'
+  # end
+  resources :recipes, only: :index
+
+  # Session
+  # get '/login' => 'sessions#new'
+  # post '/login' => 'sessions#create'
+  # get '/logout' => 'sessions#destroy'
+
+  # Google authentication
+  # get 'auth/:provider/callback', to: 'sessions#googleAuth'
+  # get 'auth/failure', to: redirect('/')
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
