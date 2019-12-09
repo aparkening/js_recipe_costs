abf103bfe9a5821541245d974401783219b9e48b
aparkening <aaron.parkening@gmail.com>
2019-12-09 12:05:49 -0500

    Return JSON for all routes.

--- backend/app/controllers/api/v1/recipes_controller.rb
+++ backend/app/controllers/api/v1/recipes_controller.rb
@@ -1,14 +1,10 @@
 class Api::V1::RecipesController < ApplicationController
   # before_action :require_login
 
-  # Send all recipes
+  # All records
   def index
-    recipes = Recipe.all
-
-    render json: RecipeSerializer.new(recipes).serialized_json, status: 200
-  end
-
-
+    # recipes = Recipe.all
+    
   #   if is_admin?
   #     @recipes = Recipe.all
   #     @user = User.find_by(id: params[:user_id])
@@ -20,196 +16,164 @@ class Api::V1::RecipesController < ApplicationController
   #     end
   #   else
   #     redirect_non_users      
-  #     @user = User.find_by(id: params[:user_id])
+      user = User.find_by(id: params[:user_id])
 
-  #     if params[:search]
-  #       # If search, find results
-	# 	    @recipes = Recipe.users_recipes(@user).where('name LIKE ?', "%#{params[:search]}%").order('id DESC')
-  #     else
-  #       # Show everything
-  #       @recipes = @user.recipes.includes(:recipe_ingredients) 
-  #     end
+      if params[:search]
+        # If search, find results
+		    recipes = Recipe.users_recipes(user).where('name LIKE ?', "%#{params[:search]}%").order('id DESC')
+      else
+        # Show everything
+        recipes = user.recipes.includes(:recipe_ingredients) 
+      end
 
-  #     # Map ingredient costs
-  #     @recipe_costs = @recipes.map do |recipe|
-  #       new_recipe = {}
+      # Map ingredient costs
+      recipe_costs = recipes.map do |recipe|
+        new_recipe = {}
 
-  #       new_recipe[:id] = recipe.id
-  #       new_recipe[:user_id] = recipe.user_id
-  #       new_recipe[:name] = recipe.name
-  #       new_recipe[:servings] = recipe.servings
+        new_recipe[:id] = recipe.id
+        new_recipe[:user_id] = recipe.user_id
+        new_recipe[:name] = recipe.name
+        new_recipe[:servings] = recipe.servings
 
-  #       # Get costs per ingredient
-  #       new_recipe[:ingredients] = recipe.recipe_ingredients.map { |ingredient| CombinedIngredient.new(ingredient) }
+        # Get costs per ingredient
+        new_recipe[:ingredients] = recipe.recipe_ingredients.map { |ingredient| CombinedIngredient.new(ingredient) }
 
-  #       # Calculate total cost
-  #       # cost << recipe.total_cost(recipe_ingredients)
-  #       new_recipe[:total_cost] = recipe.total_cost(new_recipe[:ingredients])
+        # Calculate total cost
+        # cost << recipe.total_cost(recipe_ingredients)
+        new_recipe[:total_cost] = recipe.total_cost(new_recipe[:ingredients])
 
-  #       # Calculate cost per serving
-  #       # cost << recipe.cost_per_serving(recipe.total_cost(recipe_ingredients)) if recipe.servings
-  #       new_recipe[:cost_per_serving] = recipe.cost_per_serving(new_recipe[:total_cost]) if recipe.servings
+        # Calculate cost per serving
+        # cost << recipe.cost_per_serving(recipe.total_cost(recipe_ingredients)) if recipe.servings
+        new_recipe[:cost_per_serving] = recipe.cost_per_serving(new_recipe[:total_cost]) if recipe.servings
 
-  #       new_recipe
-  #     end 
+        new_recipe
+      end 
 
-  #     # binding.pry
-  #     # @recipes = Recipe.recipes_costs(@user)
-  #   end
-  # end
-
+      render json: RecipeSerializer.new(recipes).serialized_json, status: 200  
+  end
 
   # Display user's recipes by ingredient
   def by_ingredient
-    redirect_non_users
+    # redirect_non_users
         
-    @user = User.find_by(id: params[:user_id])
+    user = User.find_by(id: params[:user_id])
   
     # If ingredient exists, find recipes that use it
     if Ingredient.exists?(params[:id])
-      @ingredient = Ingredient.find(params[:id])
-      if is_admin?
-        # Show all recipes from ingredient for admins
-        @recipes = Recipe.recipes_of_ingredient(params[:id])
-      else
+      ingredient = Ingredient.find(params[:id])
+      # if is_admin?
+      #   # Show all recipes from ingredient for admins
+      #   @recipes = Recipe.recipes_of_ingredient(params[:id])
+      # else
         # Only show user's recipes
-        @recipes = @user.recipes.recipes_of_ingredient(params[:id])
-      end
+        recipes = user.recipes.recipes_of_ingredient(params[:id])
+      # end
     else
-      flash[:alert] = "That ingredient wasn't found."
-
+      # flash[:alert] = "That ingredient wasn't found."
       # Else show all users' recipes
-      @recipes = @user.recipes
+      recipes = user.recipes
     end
 
-    render 'index'
+    render json: RecipeSerializer.new(recipes).serialized_json, status: 200
   end
 
 
   # Display record
   def show
-    redirect_non_users
-    @user = User.find_by(id: params[:user_id])
+    # redirect_non_users
+    user = User.find_by(id: params[:user_id])
     
     # Require authorization
-    require_authorization(@user)
+    # require_authorization(@user)
 
     # Search all recipes for admin; subset for user
-    if is_admin?
-      @recipe = Recipe.find_by(id: params[:id])
-    else 
-      @recipe = @user.recipes.find_by(id: params[:id])
-    end
+    # if is_admin?
+    #   @recipe = Recipe.find_by(id: params[:id])
+    # else 
+      recipe = user.recipes.find_by(id: params[:id])
+    # end
 
     # If recipe exists, iterate through ingredients to calculate each cost and combine into total cost and cost per serving.
-    if @recipe
+    if recipe
       # Map costs for each ingredient
-      @recipe_ingredients = @recipe.recipe_ingredients.map { |ingredient| CombinedIngredient.new(ingredient) }
+      @recipe_ingredients = recipe.recipe_ingredients.map { |ingredient| CombinedIngredient.new(ingredient) }
 
       # Total recipe cost
-      @recipe_total_cost = @recipe.total_cost(@recipe_ingredients)
+      @recipe_total_cost = recipe.total_cost(recipe_ingredients)
       
       # Cost per serving
       @recipe_cost_per_serving = @recipe.cost_per_serving(@recipe_total_cost) if @recipe.servings
+
+      options = {
+        include: [@recipe_ingredients, @recipe_total_cost, @recipe_cost_per_serving]
+      }
+      render json: RecipeSerializer.new(recipe, options).serialized_json
     else
-      flash[:alert] = "Recipe not found."
-      redirect_to user_recipes_path(@user)
+      render json: { message: 'Recipe not found' }
     end
-  end
-
-  # Display new form
-  def new
-    redirect_non_users
-    @user = User.find_by(id: params[:user_id])
-
-    @recipe = Recipe.new(user_id: params[:user_id])
-      
-    # Display 10 ingredient fields
-    10.times{ @recipe.recipe_ingredients.build() }
   end
 
   # Create new
   def create
-    redirect_non_users
-    @user = User.find_by(id: params[:user_id])
+    # redirect_non_users
+    user = User.find_by(id: params[:user_id])
 
     # Ensure current user can create for user
-    require_authorization(@user)
+    # require_authorization(@user)
 
     # Create recipe
-    @recipe = @user.recipes.build(recipe_params)
+    recipe = user.recipes.build(recipe_params)
 
-    if @recipe.save
-      redirect_to user_recipe_path(@user, @recipe)
+    if recipe.save
+      render json: RecipeSerializer.new(recipe).serialized_json, status: 200
     else
       # flash[:error] = @recipe.errors.full_messages
       # redirect_to new_user_recipe_path(user, recipe)
-      render :new
+      render json: { message: 'Recipe error' }
     end
-  end
-
-  # Display update form
-  def edit
-    redirect_non_users
-    @user = User.find_by(id: params[:user_id])
-
-    # Require authorization
-    require_authorization(@user)
-
-    # Search all recipes for admin; subset for user
-    if is_admin?
-      @recipe = Recipe.find_by(id: params[:id])
-    else 
-      @recipe = @user.recipes.find_by(id: params[:id])
-    end
-
-    # Add two blank ingredients to add more on edit screen
-    2.times{ @recipe.recipe_ingredients.build() }
-
-    redirect_to user_recipes_path(@user), alert: "Recipe not found." if @recipe.nil?
   end
 
   # Update record
   def update
-    redirect_non_users
-    @user = User.find_by(id: params[:user_id])
+    # redirect_non_users
+    user = User.find_by(id: params[:user_id])
     
     # Require authorization
-    require_authorization(@user)
+    # require_authorization(@user)
 
-    @recipe = Recipe.find(params[:id])
-    @recipe.update(recipe_params)
+    recipe = Recipe.find(params[:id])
+    recipe.update(recipe_params)
 
-    if @recipe.save
-      flash[:success] = "Success! Recipe updated."
-      redirect_to user_recipe_path(@user, @recipe)
+    if recipe.save
+      render json: RecipeSerializer.new(recipe).serialized_json, status: 200
     else
-      # flash[:error] = recipe.errors.full_messages
-      # redirect_to edit_user_recipe_path(user, recipe)
-      render :edit
+      render json: { message: 'Recipe error' }
     end
   end
 
   # Import CSVs
   # user_recipes_import_path
   def import
-    redirect_non_users
+    # redirect_non_users
     
     user = User.find_by(id: params[:user_id])
-    require_authorization(user)
+
+    # require_authorization(user)
 
     Recipe.import(params[:file], user)
+    recipes = user.recipes
+    # recipes = user.recipes.includes(:recipe_ingredients) 
 
-    redirect_to user_recipes_path(user), notice: "Success! File imported."
+    render json: RecipeSerializer.new(recipes).serialized_json, status: 200
   end
 
   # Delete record
   def destroy
-    redirect_non_users
+    # redirect_non_users
     user = User.find_by(id: params[:user_id])
 
     # Require authorization
-    require_authorization(user)
+    # require_authorization(user)
 
     recipe = Recipe.find(params[:id])
     
@@ -219,8 +183,8 @@ class Api::V1::RecipesController < ApplicationController
     end
 
     recipe.destroy
-    flash[:notice] = "Recipe deleted."
-    redirect_to user_recipes_path(user)
+
+    render json: {recipeId: recipe.id}, status: 200
   end
 
   private
