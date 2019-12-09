f58ecef27460c02569205f9535c694c7de536288
aparkening <aaron.parkening@gmail.com>
2019-12-09 17:09:10 -0500

    Add placeholders for csrf cookie and validation methods.

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::API
+  # include ActionController::Cookies
+  # include ActionController::RequestForgeryProtection
+
+  # protect_from_forgery with: exception
+
+  # before_action :set_csrf_cookie
+
+
+  ### Write validation methods from https://www.youtube.com/watch?v=qjtht03t7z4&feature=youtu.be @ 1 hour
+
+
+  private
+
+  def set_csrf_cookie
+    cookies["CSRF-TOKEN"] = form_authenticity_token
+  end
+
 end
