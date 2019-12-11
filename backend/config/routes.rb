1343680f5841bca62732833056ff57c7ff5c217e
aparkening <aaron.parkening@gmail.com>
2019-12-11 11:19:16 -0500

    Remove get /login from routes.

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     post 'ingredients/import'
 
     # Session
-    get '/login' => 'sessions#new'
+    # get '/login' => 'sessions#new'
     post '/login' => 'sessions#create'
     get '/logout' => 'sessions#destroy'
 
