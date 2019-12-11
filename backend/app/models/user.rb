000f8aedbe0ae1f26dfd093990db6d71b8821473
aparkening <aaron.parkening@gmail.com>
2019-12-11 14:51:21 -0500

    Add jwt blacklist and devise/jwt items to user model.

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,5 +1,15 @@
 class User < ApplicationRecord
-  has_secure_password
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # devise :database_authenticatable, :registerable,
+  #        :recoverable, :rememberable, :validatable
+
+  devise :database_authenticatable,
+        :registerable,
+        :jwt_authenticatable,
+        jwt_revocation_strategy: JWTBlacklist
+
+  # has_secure_password
 
   # Relationships
   has_many :recipes
