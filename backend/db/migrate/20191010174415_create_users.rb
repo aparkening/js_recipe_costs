b3de67ba2c68798a64b14d493d61a814071a8d48
aparkening <aaron.parkening@gmail.com>
2019-12-12 07:37:18 -0500

    Remove password and email from users to enable devise migration. Use email as main string in devise. Move jwt_blacklists after devise migration.

--- backend/db/migrate/20191010174415_create_users.rb
+++ backend/db/migrate/20191010174415_create_users.rb
@@ -1,12 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.string :name, null: false, default: ""
-      t.string :password_digest
+      t.string :name
+      # t.string :password_digest
       t.boolean :admin, default: false
       t.string :organization
-      t.string :uid
-      t.string :email, null: false, default: ""
+      # t.string :email
 
       t.timestamps
     end
